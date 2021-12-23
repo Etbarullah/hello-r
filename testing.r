c141ad591f6a1fcad68e3c2884a58a0e507fed34
Etbarullah <miqbalkhaan@gmail.com>
2021-12-23 14:59:48 +0500

    few lines have been added

--- testing.r
+++ testing.r
@@ -1,3 +1,11 @@
-This is from Github
+#This is from Github
 print("This is from Github")
 
+ name = "iqbal"
+ version = ("R-stuodio in writing")
+ 
+ print(name)
+ print("These lines have been written in R-studio")
+ 
+ 
+ 
